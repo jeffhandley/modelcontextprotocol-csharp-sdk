14254d6ff30f4eb81b3282bea4c364ad5085ee8d
Jeff Handley <jeffhandley@users.noreply.github.com>
2025-03-26 19:49:12 -0700

    Set up central artifacts folder

---
+++ src/ModelContextProtocol.TestPackagePublishingIgnore/README.md
@@ -0,0 +1,3 @@
+# ModelContextProtocol - Test Package Publishing - Ignore
+
+This package is used to verify publishing to NuGet.org using the 'ModelContextProtocol' ID prefix. It contains no usable code.
---
+++ src/ModelContextProtocol.TestPackagePublishingIgnore/VerifyPackageCanPublishToNuGet.cs
@@ -0,0 +1,9 @@
+﻿namespace ModelContextProtocol.TestPackagePublishingIgnore
+{
+    /// <summary>
+    /// This class is used to verify that the package can be published to NuGet without issues.
+    /// </summary>
+    public static class VerifyPackageCanPublishToNuGet
+    {
+    }
+}
